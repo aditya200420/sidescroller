3a877c972e9a678656ba7c3ea6643191a3e175c0
aditya singh <110594057+aditya200420@users.noreply.github.com>
2022-11-10 05:56:00 -0800

    Add files via upload

---
+++ EnemyController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class EnemyController : GazePointer
+{
+    public GameObject particleEffect;
+    public Animator enemyModel;
+    public float speed;
+    Vector3 endPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        endPos = 1.5f * (transform.position - Vector3.zero).normalized;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, endPos, speed * Time.deltaTime);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            Attack();
+    }
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        base.OnPointerDown(eventData);
+        Death();
+    }
+    public void Death()
+    {
+        particleEffect.SetActive(true);
+        particleEffect.transform.SetParent(null);
+        Destroy(gameObject);
+    }
+    public void Attack()
+    {
+        enemyModel.SetTrigger("attack");
+    }
+       
+}
